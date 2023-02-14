7a4c4b3e42a6871e1792cceb66ddc1c349e81f2d
Daniel Costa <danielfurtadocosta@gmail.com>
2023-02-14 22:11:32 +0000

    add feedback components to app wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { Key } from 'react';
 import { ReactComponent as CheckoutLogo } from './logo.svg';
+
+import FeedbackForm from './components/feedback-form/feedback-form';
+import FeedbackResults from './components/feedback-results/feedback-results';
 
 import './App.scss';
 
@@ -9,49 +13,9 @@ const App = () => {
         <CheckoutLogo />
       </header>
       <main className='Main--container'>
-        <form className='Form'>
-          <h1>Feedback Form</h1>
-          <div className='Form--group'>
-            <label htmlFor='name'>Name *</label>
-            <input 
-              type='text'
-              id='name'
-              placeholder='Name' 
-              name='name' 
-            />
-          </div>
-          <div className='Form--group'>
-            <label htmlFor='email'>Email *</label>
-            <input 
-              type='text' 
-              id='email'
-              placeholder='Email' 
-              name='email' 
-            />
-          </div>
-          <div className='Form--group'>
-            <label htmlFor='rating'>Rating *</label>
-            <input 
-              type='text' 
-              id='rating'
-              placeholder='Rating (1-5 stars)' 
-              name='rating' 
-            />
-          </div>
-          <div className='Form--group'>
-            <label htmlFor='comment'>Comment *</label>
-            <input 
-              type='textarea' 
-              className='Form--textarea'
-              id='comment'
-              placeholder='Comment' 
-              name='comment' 
-            />
-          </div>
-          <div>
-            <button type='submit' className='Button--primary'>Submit</button>
-          </div>
-        </form>
+        <FeedbackForm />
+        <hr />
+        <FeedbackResults />
       </main>
     </div>
   );
