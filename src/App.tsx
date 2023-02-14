8b6679a27adf13ebb77f66bfaf2b90deb1f07a51
Daniel Costa <danielfurtadocosta@gmail.com>
2023-02-14 00:30:43 +0000

    add checkout.com logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { ReactComponent as CheckoutLogo } from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Checkout.com
+        <CheckoutLogo />
       </header>
+      <main className='Main-container'>
+        <p>
+          <button className='Button-primary'>Click me!</button>
+        </p>
+      </main>
     </div>
   );
 }
