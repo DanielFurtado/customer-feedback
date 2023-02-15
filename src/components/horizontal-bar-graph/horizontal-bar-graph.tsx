5bf1de33c47fe071332fcbe7343a47ee69f5c345
Daniel Costa <danielfurtadocosta@gmail.com>
2023-02-15 09:58:21 +0000

    import styled components to horizontal bar graph

--- src/components/horizontal-bar-graph/horizontal-bar-graph.tsx
+++ src/components/horizontal-bar-graph/horizontal-bar-graph.tsx
@@ -1,26 +1,34 @@
 import { FC, Fragment } from 'react';
 import { CustomerRatings } from '../feedback-results/feedback-results';
 
-import './horizontal-bar-graph.styles.scss';
+import {
+  BarWrapper,
+  BarLabel,
+  BarPercentage,
+  BarRatingContainer,
+  HorizontalBar,
+} from './horizontal-bar-graph.styles';
 
 type BarGraphItemProps = {
   rating: CustomerRatings;
 };
 
 const HorizontalBarGraph: FC<BarGraphItemProps> = ({rating}) => {
-  const { barClass, label, percentage } = rating;
+  const { barColour, label, startTime, percentage } = rating;
   
   return (
     <Fragment>
-      <div className='Bar--rating-container'>
-        <div className='Bar--label'>{label}</div>
-        <div className='Bar--wrapper'>
-            <div className={barClass}>
-              <div className='Bar'></div>
-            </div>
-        </div>
-        <div className='Bar--percentage'>{percentage}%</div>
-      </div>
+      <BarRatingContainer>
+        <BarLabel>{label}</BarLabel>
+        <BarWrapper>
+          <HorizontalBar 
+            barColour={barColour} 
+            startTime={startTime} 
+            percentage={percentage}
+          ></HorizontalBar>
+        </BarWrapper>
+        <BarPercentage>{percentage}%</BarPercentage>
+      </BarRatingContainer>
     </Fragment>
   );
 };
