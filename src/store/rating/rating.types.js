862fe0fff13245416c89393e1c95ae262bb186f8
Daniel Costa <danielfurtadocosta@gmail.com>
2023-02-20 12:38:55 +0000

    Revert "Revert "add app wrapper for styled components""
    This reverts commit db60013b3aef59f4c9cd7af616203d616a92b9fe.

---
+++ src/store/rating/rating.types.js
@@ -0,0 +1,5 @@
+const RATING_ACTION_TYPES = {
+  SET_RATING: 'rating/SET_RATING',
+};
+
+export default RATING_ACTION_TYPES;
---
+++ src/store/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
+import ratingReducer from './rating/rating.reducer';
+
+export const store = configureStore({
+  reducer: {
+    feedbackList: ratingReducer
+  }
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
